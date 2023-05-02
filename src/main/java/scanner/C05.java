6590f0e877f328acb918df34e3dd46d854af1775
Muhammed Musab Demir <mmusabdemirr@gmail.com>
2023-05-02 17:34:50 +0300

    calisma

---
+++ src/main/java/scanner/C05.java
@@ -0,0 +1,14 @@
+package scanner;
+
+public class C05 {
+    /*
+      int veri türünün Maximum ve Minimum değerlerini gösteren kodu konsola yazınız.
+     */
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+
+    }
+}
